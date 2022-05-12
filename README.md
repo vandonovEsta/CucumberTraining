710b7642ca361487aca24de6975f30c94e43caad
Viktor <vandonov@estafet.com>
2022-05-12 16:48:17 +0300

    added docmentation

--- README.md
+++ README.md
@@ -4,3 +4,12 @@ Exercise Cucumber with java.
 Create a simple class to print Hello $name!
 or Hello World! if no name is provided.
 Write simple tests for the HelloWorldApp in Cucumber.
+
+StringHelper
+------------
+
+Added a helper class to assist with string operations.
+As of now it just removes unwanted symbols and counts words.
+There is an unfinished implementation of breaking the string to words.
+
+More functionality to be added.
--- src/main/java/helpers/StringHelper.java
+++ src/main/java/helpers/StringHelper.java
@@ -4,14 +4,26 @@ public class StringHelper {
     String[] words;
     private boolean isCaseSensitive;
 
+    /**
+     * @param isCaseSensitive used in String comparison
+     */
     public StringHelper(boolean isCaseSensitive) {
         this.isCaseSensitive = isCaseSensitive;
     }
 
+    /**
+     * Sets case-sensitive to true by default
+     */
     public StringHelper() {
         this(true);
     }
 
+    /**
+     * Compare two strings based on case-sensitive option
+     * @param firstString
+     * @param secondString
+     * @return
+     */
     //TODO: extend this for words and for sentences
     public boolean compare(String firstString, String secondString) {
         if (!isCaseSensitive) {
@@ -29,6 +41,7 @@ public class StringHelper {
         isCaseSensitive = caseSensitive;
     }
 
+    //TODO: Reword this to trim extra characters from words
     public String[] splitSentenceToWords(String sentence) {
         words = sentence.split("\\s");
         return words;
@@ -36,10 +49,15 @@ public class StringHelper {
 
     public String removeAllCharacters(String input, char replacesMe) {
 
-
         return input.replaceAll(String.valueOf(replacesMe), "");
     }
 
+    /**
+     * @param input
+     * @param replaceMe
+     * @return String without the specified characters
+     * @throws Exception
+     */
     public String removeAllCharacters(String input, String replaceMe) throws Exception {
         if (replaceMe.length() > 1) {
             throw new Exception("More han 1 character are not allowed!");
@@ -50,6 +68,11 @@ public class StringHelper {
         }
     }
 
+    /**
+     * Escapes special characters
+     * @param input
+     * @return
+     */
     private String escapeSpecialCharacter(String input) {
         String escapedCharacter;
         switch (input) {
@@ -75,6 +98,13 @@ public class StringHelper {
         return escapedCharacter;
     }
 
+    /**
+     * Removes the first occurrence
+     * @param input
+     * @param replaceMe
+     * @return
+     * @throws Exception
+     */
     public String removeFirst(String input, String replaceMe) throws Exception {
         if (replaceMe.length() > 1) {
             throw new Exception("More han 1 character are not allowed!");
@@ -83,6 +113,11 @@ public class StringHelper {
         }
     }
 
+    /**
+     * Splits a string to words and counts them
+     * @param countMyWords
+     * @return
+     */
     public int countWords(String countMyWords) {
         String[] wordArray = countMyWords.split(" ");
         return wordArray.length;
