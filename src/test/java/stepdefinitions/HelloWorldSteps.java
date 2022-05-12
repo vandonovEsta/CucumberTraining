6fd2bacb8480b7ec38907ad844e3d0af1a286791
Viktor <vandonov@estafet.com>
2022-05-12 10:27:54 +0300

    cleaning of steps file

--- src/test/java/stepdefinitions/HelloWorldSteps.java
+++ src/test/java/stepdefinitions/HelloWorldSteps.java
@@ -3,32 +3,34 @@ package stepdefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.example.HelloWorldApp;
 import org.junit.Assert;
 
-import static org.example.HelloWorldApp.*;
+import static org.example.HelloWorldApp.greet;
 
 public class HelloWorldSteps {
 
     private String greeting;
     private String name;
+
     @Given("No specific name is given")
     public void no_specific_name_is_given() {
         // Write code here that turns the phrase above into concrete actions
         name = "";
     }
+
     @When("The Hello World app is called")
     public void the_hello_world_app_is_called() {
         // Write code here that turns the phrase above into concrete actions
-        if(name == null || name.isEmpty()){
+        if (name == null || name.isEmpty()) {
             greeting = greet();
         } else {
             greeting = greet(name);
         }
     }
+
     @Then("The Hello World app greeting should be returned.")
     public void the_hello_world_greeting_should_be_returned() {
-        if(name == null || name == "") {
+        if (name == null || name == "") {
             Assert.assertEquals(greeting, "Hello World!");
         } else {
             Assert.assertEquals(greeting, "Hello " + name + "!");
@@ -37,7 +39,7 @@ public class HelloWorldSteps {
     }
 
     @Given("Greg enters the room")
-    public void greg_enters_the_room(){
+    public void greg_enters_the_room() {
         name = "Greg";
     }
 }
